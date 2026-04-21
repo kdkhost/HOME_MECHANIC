9803175235ee19333ce14e97585fcfa2301540b5
HomeMechanic Developer <dev@homemechanic.com.br>
2026-04-21 14:46:26 -0300

    fix: adicionar import do Log facade no UsersController

--- app/Modules/Users/Controllers/UsersController.php
+++ app/Modules/Users/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use App\Helpers\FileUploadHelper;
 
 class UsersController extends Controller
